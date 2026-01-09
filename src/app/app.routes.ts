9384436ea32996b55b7f5e04c8d9d5f89835691c
el-rabies <eleoraby@gmail.com>
2026-01-09 10:41:07 -0500

    basic router testing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { DocumentationPageComponent } from './documentation-page/documentation-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomePageComponent, pathMatch: 'full'},
+    {path: 'documentation', component: DocumentationPageComponent},
+    {path: '**', component: PageNotFoundComponent}
+];
---
+++ src/app/documentation-page/documentation-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-documentation-page',
+  imports: [],
+  templateUrl: './documentation-page.component.html',
+  styleUrl: './documentation-page.component.scss'
+})
+export class DocumentationPageComponent {
+
+}
