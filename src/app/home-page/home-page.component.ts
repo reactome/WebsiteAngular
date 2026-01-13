f671fcaa253038f510db4d2f37a921686bc17c98
el-rabies <eleoraby@gmail.com>
2026-01-13 10:15:05 -0500

    Set template for homepage

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { SearchBarComponent } from "../search-bar/search-bar.component";
+import { HomeShortcutsComponent } from "./home-shortcuts/home-shortcuts.component";
+import { HomeSpotlightComponent } from "./home-spotlight/home-spotlight.component";
+import { HomeWhyReactomeComponent } from "./home-why-reactome/home-why-reactome.component";
+import { HomeLatestNewsComponent } from "./home-latest-news/home-latest-news.component";
+import { HomeStatsComponent } from "./home-stats/home-stats.component";
+import { HomeHelpComponent } from "./home-help/home-help.component";
+import { HomeApiDataComponent } from "./home-api-data/home-api-data.component";
+import { HomeRelatedComponent } from "./home-related/home-related.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [SearchBarComponent, HomeShortcutsComponent, HomeSpotlightComponent, HomeWhyReactomeComponent, HomeLatestNewsComponent, HomeStatsComponent, HomeHelpComponent, HomeApiDataComponent, HomeRelatedComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
