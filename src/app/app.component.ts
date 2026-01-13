27076a44ad66eb28e21eba6d776aa8416d76a549
el-rabies <eleoraby@gmail.com>
2026-01-13 09:36:26 -0500

    Added "Scroll to Top Button"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { NavigationBarComponent } from "./navigation-bar/navigation-bar.componen
 import { InfoFooterComponent } from "./info-footer/info-footer.component";
 import { copyrightFooterComponent } from "./copyright-footer/copyright-footer.component";
 import { CiteUsComponent } from "./cite-us/cite-us.component";
+import { ScrollToTopComponent } from "./scroll-to-top/scroll-to-top.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLinkWithHref, RouterLinkActive, NavigationBarComponent, InfoFooterComponent, copyrightFooterComponent, CiteUsComponent],
+  imports: [RouterOutlet, RouterLinkWithHref, RouterLinkActive, NavigationBarComponent, InfoFooterComponent, copyrightFooterComponent, CiteUsComponent, ScrollToTopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/scroll-to-top/scroll-to-top.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-scroll-to-top',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './scroll-to-top.component.html',
+  styleUrl: './scroll-to-top.component.scss'
+})
+export class ScrollToTopComponent {
+  // Source - https://stackoverflow.com/a
+  // Posted by Ashish Dahiya
+  // Retrieved 2026-01-12, License - CC BY-SA 4.0
+
+  scrollToTop(): void {
+    window.scrollTo(0, 0);
+  }
+
+
+  windowScrolled = false;
+
+  ngOnInit() {
+    window.addEventListener('scroll', () => {
+      this.windowScrolled = window.pageYOffset !== 0;
+    });
+  }
+
+}
