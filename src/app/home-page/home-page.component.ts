64d019577245c1dbac023c4935dc1baa16b759bd
el-rabies <eleoraby@gmail.com>
2026-01-14 15:41:42 -0500

    implemented button component

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,22 +1,33 @@
 import { Component } from '@angular/core';
-import { SearchBarComponent } from "../search-bar/search-bar.component";
-import { HomeShortcutsComponent } from "./home-shortcuts/home-shortcuts.component";
-import { HomeSpotlightComponent } from "./home-spotlight/home-spotlight.component";
-import { HomeWhyReactomeComponent } from "./home-why-reactome/home-why-reactome.component";
-import { HomeLatestNewsComponent } from "./home-latest-news/home-latest-news.component";
-import { HomeStatsComponent } from "./home-stats/home-stats.component";
-import { HomeHelpComponent } from "./home-help/home-help.component";
-import { HomeApiDataComponent } from "./home-api-data/home-api-data.component";
-import { HomeRelatedComponent } from "./home-related/home-related.component";
-import { TileComponent } from "../reactome-components/tile/tile.component";
+import { SearchBarComponent } from '../search-bar/search-bar.component';
+import { HomeShortcutsComponent } from './home-shortcuts/home-shortcuts.component';
+import { HomeSpotlightComponent } from './home-spotlight/home-spotlight.component';
+import { HomeWhyReactomeComponent } from './home-why-reactome/home-why-reactome.component';
+import { HomeLatestNewsComponent } from './home-latest-news/home-latest-news.component';
+import { HomeStatsComponent } from './home-stats/home-stats.component';
+import { HomeHelpComponent } from './home-help/home-help.component';
+import { HomeApiDataComponent } from './home-api-data/home-api-data.component';
+import { HomeRelatedComponent } from './home-related/home-related.component';
+import { TileComponent } from '../reactome-components/tile/tile.component';
+import { ButtonComponent } from '../reactome-components/button/button.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [SearchBarComponent, HomeShortcutsComponent, HomeSpotlightComponent, HomeWhyReactomeComponent, HomeLatestNewsComponent, HomeStatsComponent, HomeHelpComponent, HomeApiDataComponent, HomeRelatedComponent, TileComponent],
+  imports: [
+    SearchBarComponent,
+    HomeShortcutsComponent,
+    HomeSpotlightComponent,
+    HomeWhyReactomeComponent,
+    HomeLatestNewsComponent,
+    HomeStatsComponent,
+    HomeHelpComponent,
+    HomeApiDataComponent,
+    HomeRelatedComponent,
+    TileComponent,
+    ButtonComponent,
+  ],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
+  styleUrl: './home-page.component.scss',
 })
-export class HomePageComponent {
-
-}
+export class HomePageComponent {}
--- src/app/reactome-components/button/button.component.ts
+++ src/app/reactome-components/button/button.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
+  standalone: true,
   selector: 'app-button',
   imports: [],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss'
 })
 export class ButtonComponent {
+  @Input() variant: 'dark' | 'light' = 'light';
 
+  @Input() onClick: () => void = () => {};
+
+  @Input() shape: 'circle' | 'square' = 'square';
+
+  @Input() size: 'small' | 'medium' | 'large' = 'medium';
+
+  get buttonVariant(): string {
+    return this.variant === 'dark' ? 'dark-button' : 'light-button';
+  }
+
+  get buttonShape(): string {
+    return this.shape === 'circle' ? 'circle-button' : 'square-button';
+  }
+
+  get buttonSize(): string {
+    return this.size === 'small' ? 'small-button' :
+           this.size === 'large' ? 'large-button' : 'medium-button';
+  }
 }
