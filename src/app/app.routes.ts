496908effe1363c6d6ce92e59d389b438a6c6300
el-rabies <eleoraby@gmail.com>
2026-02-04 16:23:30 -0500

    fixed routing for websiteangular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     { path: 'PathwayBrowser', loadChildren: () => import('../../projects/pathway-browser/src/app/app-routing.module').then(m => m.AppRoutingModule) },
-    { path: '', loadChildren: () => import('../../projects/website-angular/src/app/app-routing.module').then(m => m.AppRoutingModule) },
+    { path: '', loadChildren: () => import('../../projects/website-angular/src/app/app.routes').then(m => m.routes) },
 ];
