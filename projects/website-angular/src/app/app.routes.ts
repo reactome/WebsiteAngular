bb63e4ee97c65db8f7d0d4a93909197ac60a0361
el-rabies <eleoraby@gmail.com>
2026-02-10 10:38:41 -0500

    lazy routing

--- projects/website-angular/src/app/app.routes.ts
+++ projects/website-angular/src/app/app.routes.ts
@@ -1,29 +1,23 @@
 import { Routes } from '@angular/router';
-import { HomePageComponent } from './home-page/home-page.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
-import { PageComponent } from './page/page.component';
-import { ArticlePageComponent } from './article/article-page/article-page.component';
-import { ArticleComponent } from './article/article/article.component';
 
 export const routes: Routes = [
     //Home Page
-    { path: '', component: HomePageComponent, pathMatch: 'full' },
+    { path: '', loadComponent: () => import('./home-page/home-page.component').then(m => m.HomePageComponent), pathMatch: 'full' },
 
     /* Non - CMS Pages Below this Line */
     //News Pages
-    { path: 'about/news', component: ArticlePageComponent, pathMatch: 'full' },
-    { path: 'about/news/:slug', component: ArticleComponent, pathMatch: 'full' },
+    { path: 'about/news', loadComponent: () => import('./article/article-page/article-page.component').then(m => m.ArticlePageComponent), pathMatch: 'full' },
+    { path: 'about/news/:slug', loadComponent: () => import('./article/article/article.component').then(m => m.ArticleComponent), pathMatch: 'full' },
 
     //Reactome Research Spotlights
-    { path: 'content/reactome-research-spotlight', component: ArticlePageComponent, pathMatch: 'full' },
-    { path: 'content/reactome-research-spotlight/:slug', component: ArticleComponent, pathMatch: 'full' },
+    { path: 'content/reactome-research-spotlight', loadComponent: () => import('./article/article-page/article-page.component').then(m => m.ArticlePageComponent), pathMatch: 'full' },
+    { path: 'content/reactome-research-spotlight/:slug', loadComponent: () => import('./article/article/article.component').then(m => m.ArticleComponent), pathMatch: 'full' },
 
     //404 Page
-    { path: '404', component: PageNotFoundComponent, pathMatch: 'full' },
+    { path: '404', loadComponent: () => import('./page-not-found/page-not-found.component').then(m => m.PageNotFoundComponent), pathMatch: 'full' },
     /* Non - CMS Pages Above this Line */
 
     //CMS Pages
-    { path: '**', component: PageComponent },
+    { path: '**', loadComponent: () => import('./page/page.component').then(m => m.PageComponent) },
     
 ];
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,5 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-// Import custom elements definitions
-import './app/app.elements';
-
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
