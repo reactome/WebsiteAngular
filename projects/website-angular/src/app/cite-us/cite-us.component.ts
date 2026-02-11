64d1b209e60a997a72a1a45066552c8ac2cc8b3e
el-rabies <eleoraby@gmail.com>
2026-02-11 10:49:20 -0500

    Cite us component

--- projects/website-angular/src/app/cite-us/cite-us.component.ts
+++ projects/website-angular/src/app/cite-us/cite-us.component.ts
@@ -1,54 +1,21 @@
-import { Component } from '@angular/core';
-import { CommonModule, NgIf } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { NgIf } from '@angular/common';
 import { MatIcon } from "@angular/material/icon";
-import { ButtonComponent } from "../reactome-components/button/button.component";
+import { CitationService } from "../../services/citation.service";
 
 @Component({
   selector: 'app-cite-us',
   standalone: true,
-  imports: [NgIf, MatIcon, ButtonComponent],
+  imports: [NgIf, MatIcon],
   templateUrl: './cite-us.component.html',
   styleUrl: './cite-us.component.scss'
 })
 export class CiteUsComponent {
   showText: boolean = false;
-  showModal: boolean = false;
-  citationType: 'bibtex' | 'ris' | 'plaintext' = 'bibtex';
+  
+  private citation: CitationService = inject(CitationService);
 
-  citationText: string = 'TODO: Citation text';
-  mailToLink: string = '';
-
-  ngOnInit() {
-    this.generateCitation();
-    this.generateMailto();
-  }
-
-  download () {
-    var blob = new Blob([this.citationText], { type: 'text/plain;charset=utf-8' });
-    var link = document.createElement('a');
-    link.href = window.URL.createObjectURL(blob);
-    let filename = 'reactome_citation.';
-    if (this.citationType === 'bibtex') {
-      filename += 'bib';
-    } else if (this.citationType === 'ris') {
-      filename += 'ris';
-    } else {
-      filename += 'txt';
-    }
-    link.download = filename;
-    link.click();
-  }
-
-  copyToClipboard() {
-    navigator.clipboard.writeText(this.citationText);
-  }
-
-  generateMailto() {
-    const subject = 'Reactome Citation';
-    this.mailToLink = `mailto:?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(this.citationText)}`;
-  }
-
-  generateCitation() {
-    //TODO: Generate citation based on current route and citationType
+  openCitationDialog() {
+    this.citation.openDialog();
   }
 }
