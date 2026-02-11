371c0f8d58a8145acbefbcf8ddc11bf5819c29b8
el-rabies <eleoraby@gmail.com>
2026-02-11 14:37:50 -0500

    fix: removed unsued import

--- projects/website-angular/src/services/content.service.ts
+++ projects/website-angular/src/services/content.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, map, catchError, of, tap } from 'rxjs';
-import { marked } from 'marked';
 import parseFrontmatter from '../utils/parseFrontmatter';
 import { Article, ArticleIndexItem } from '../types/article';
 import truncateHtml from '../utils/truncateHtml';
