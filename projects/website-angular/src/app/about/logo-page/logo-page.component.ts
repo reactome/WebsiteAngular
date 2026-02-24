47588cd494e05a72ff146ebce69b17f38558f7a2
el-rabies <eleoraby@gmail.com>
2026-02-24 09:47:34 -0500

    fix: Put logo-page in app-page-layout

--- projects/website-angular/src/app/about/logo-page/logo-page.component.ts
+++ projects/website-angular/src/app/about/logo-page/logo-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TileComponent } from "../../reactome-components/tile/tile.component";
 import { DropdownComponent } from "../../reactome-components/dropdown/dropdown.component";
+import { PageLayoutComponent } from "../../page-layout/page-layout.component";
 
 @Component({
   selector: 'app-logo-page',
-  imports: [TileComponent, DropdownComponent],
+  imports: [TileComponent, DropdownComponent, PageLayoutComponent],
   templateUrl: './logo-page.component.html',
   styleUrl: './logo-page.component.scss'
 })
