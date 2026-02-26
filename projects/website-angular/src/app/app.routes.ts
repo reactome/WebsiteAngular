f2204e9ee27c63d345abeaebcb8a030f0d3ae2ad
el-rabies <eleoraby@gmail.com>
2026-02-26 14:00:38 -0500

    chore: removed outdated advance search route

--- projects/website-angular/src/app/app.routes.ts
+++ projects/website-angular/src/app/app.routes.ts
@@ -28,7 +28,6 @@ export const routes: Routes = [
 
     //Search Page
     { path: 'content/advanced', redirectTo: '/content/query?advanced=true' },
-    { path: 'tools/advanced', redirectTo: '/content/query?advanced=true' },
     { path: 'content/query', loadComponent: () => import('./search/search.component').then(m => m.SearchComponent) },
 
     //404 Page
