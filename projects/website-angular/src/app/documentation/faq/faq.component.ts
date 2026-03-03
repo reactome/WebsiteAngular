5953aca033b4361b1d523580ff89cf132f97490b
el-rabies <eleoraby@gmail.com>
2026-03-03 16:44:29 -0500

    Reorganized faq articles, added faq to content service

--- projects/website-angular/src/app/documentation/faq/faq.component.ts
+++ projects/website-angular/src/app/documentation/faq/faq.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { PageLayoutComponent } from "../../page-layout/page-layout.component";
+import { ContentService } from 'projects/website-angular/src/services/content.service';
 
 @Component({
   selector: 'app-faq',
@@ -8,5 +9,21 @@ import { PageLayoutComponent } from "../../page-layout/page-layout.component";
   styleUrl: './faq.component.scss'
 })
 export class FaqComponent {
-  
+  contentService = inject(ContentService);
+
+  categories:string[] = [];
+  faqIndex: Record<string, any> = {};
+
+  ngOnInit() {
+    this.contentService.getFaqIndex().subscribe({
+      next: async (result) => {
+        this.faqIndex = result;
+        this.categories = Object.keys(result);
+        console.log('FAQ Index:', this.faqIndex, "\nCategories:", this.categories);
+      },
+      error: (err) => {
+        console.error('Error fetching FAQ index:', err);
+      }
+    });
+  }
 }
