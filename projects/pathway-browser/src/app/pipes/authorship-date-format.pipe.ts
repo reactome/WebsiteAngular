d1d2294cf50ac5ca864d291b6ba7ff4cbcbb9e10
el-rabies <eleoraby@gmail.com>
2026-03-27 15:01:56 -0400

    fix: Resolved NullInjector issue what was leading to console errors and tooltip visual bugs

--- projects/pathway-browser/src/app/pipes/authorship-date-format.pipe.ts
+++ projects/pathway-browser/src/app/pipes/authorship-date-format.pipe.ts
@@ -1,17 +1,21 @@
 import {Inject, LOCALE_ID, Pipe, PipeTransform} from '@angular/core';
-import {DatePipe} from "@angular/common";
+import {formatDate} from "@angular/common";
 
 @Pipe({
   name: 'authorshipDateFormat',
   standalone: true
 })
 export class AuthorshipDateFormatPipe implements PipeTransform {
-  constructor(private datePipe: DatePipe, @Inject(LOCALE_ID) private locale: string) {
+  constructor(@Inject(LOCALE_ID) private locale: string) {
   }
 
   transform(dateTime: string) {
     if (!dateTime) return;
-    return this.datePipe.transform(dateTime);
+    try {
+      return formatDate(dateTime, 'mediumDate', this.locale);
+    } catch {
+      return;
+    }
   }
 
 }
